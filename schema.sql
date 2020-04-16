11c52e94c769d356ef2db6281620404a28d44bc4
Chance Wright <cwright489@gmail.com>
2020-04-15 21:07:01 -0600

    adding inserts into the 3 tables

--- schema.sql
+++ schema.sql
@@ -29,3 +29,19 @@ create table employee (
     references role(id)
     on delete cascade
 );
+
+insert into department (name) values ("Upper Management");
+insert into department (name) values ("Engineers");
+insert into department (name) values ("Marketing");
+insert into department (name) values ("Entry Level/Interns");
+
+insert into role (title, salary, dept_id) values ("CEO", "90000", "1");
+insert into role (title, salary, dept_id) values ("CTO", "80000", "1");
+insert into role (title, salary, dept_id) values ("Lead Progammer", "70000", "2");
+insert into role (title, salary, dept_id) values ("Programmers", "60000", "2");
+insert into role (title, salary, dept_id) values ("Sales Lead", "50000", "3");
+insert into role (title, salary, dept_id) values ("Sales Representitives", "40000", "3");
+insert into role (title, salary, dept_id) values ("Junior Developer", "30000", "4");
+insert into role (title, salary, dept_id) values ("Code Checker", "30000", "4");
+
+insert into employee (first_name, last_name, role_id) values ("Chance", "Wright", "1")
