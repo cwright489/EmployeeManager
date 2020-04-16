95d7e9267245b8f3942dc0a7610966249d280ed3
Chance Wright <cwright489@gmail.com>
2020-04-15 20:47:48 -0600

    added role table

--- schema.sql
+++ schema.sql
@@ -6,3 +6,14 @@ create table department (
     name varchar(30) not null,
     primary key(id)
 );
+
+create table role (
+    id int auto_increment,
+    title varchar(30) not null,
+    salary decimal(9.8) not null,
+    dept_id int not null,
+    primary key(id),
+    foreign key(dept_id),
+    references department(id),
+    on delete cascade
+);z
